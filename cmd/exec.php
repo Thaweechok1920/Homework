18fbeb4ece113e055a12476fabddb4216ee956a6
Thaweechok1920 <35834033+Thaweechok1920@users.noreply.github.com>
2018-02-23 21:30:21 +0700

    Update exec.php

--- cmd/exec.php
+++ cmd/exec.php
@@ -1,97 +1,54 @@
-﻿<?php
-include(configdb.php);
-include(cmdexec.php);
-
-$db = new Database();
-$str_conn = $db-getConnection();
-$str_exe = new ExecSQL($str_conn);
-$action= $_GET['cmd'];
-switch($action){
-    case select
-    $stmt = $str_exe-readAll(courses);
-    $num_row = $str_exe-rowCount(courses);
-    echo json_encode($num_row);
-    if ($num_row0){
-        $data_arr['rs'] = array();
-        foreach($stmt as $row){
-            $item = array(
-                'code'=$row['code'],
-                'name'=$row['name'],
-                'speaker_name'=$row['speaker_name'],
-                'img_path'=$row['img_path'],
-                'detail'= $row['detail'],
-                'course_outline'=$row['course_outline'],
-                'date_open'=$row['date_open'],
-                'date_end'=$row['date_end'],
-                'place'=$row['place'],
-                'seat_num'=$row['seat_num'],
-                'cost'=$row['cost'],
-                'comment'=$row['comment'],
-                'count_view_page'=$row['count_view_page'],
-                'status'=$row['status'],
-            );
-            array_push($data_arr['rs'],$item);
-        }
-            echo json_encode($data_arr);
-    }else{
-        echo json_encode(array('msg'='result not format'));
+<?php
+class ExecSQL{
+    private $conn;
+     public function __construct($str_conn){
+        $this->conn =$str_conn;
+     }
+     //ค้นหาตาราง
+     public function readAll($tablename){
+         $stmt =$this->conn->prepare("SELECT * FROM ".$tablename);
+         $stmt ->execute();
+         return $stmt;
     }
-    break;
-
-
-    case 'insert' 
-    $name = $_GET['name'];
-    $speaker_name = $_GET['speaker_name'];
-    $detail = $_GET['detail'];
-    $img_path = $_GET['img_path'];
-    $course_outline = $_GET['course_outline'];
-    $date_open = $_GET['date_open'];
-    $date_end = $_GET['date_end'];
-    $place = $_GET['place'];
-    $seat_num = $_GET['seat_num'];
-    $cost = $_GET['cost'];
-    $comment = $_GET['comment'];
-    $count_view_page = $_GET['count_view_page'];
-    $status = $_GET['status'];
-
-    $strSQL = $str_exe-insert(courses,
-    name,speaker_name,img_path,detail,course_outline,date_open,date_end,place,seat_num,cost,comment,count_view_page,status,
-    '$name','$speaker_name','$img_path','$detail','$course_outline','$date_open','$date_end','$place','$seat_num','$cost','$comment','$count_view_page','$status');
-    if($strSQL)echo json_encode(array('msg'='Insert OK'));
-    else echo json_encode(array('msg'='Can not Insert'));
-
-
-    case 'update'  
-    $name = $_GET['name'];
-    $speaker_name = $_GET['speaker_name'];
-    $detail = $_GET['detail'];
-    $img_path = $_GET['img_path'];
-    $course_outline = $_GET['course_outline'];
-    $date_open = $_GET['date_open'];
-    $date_end = $_GET['date_end'];
-    $place = $_GET['place'];
-    $seat_num = $_GET['seat_num'];
-    $cost = $_GET['cost'];
-    $comment = $_GET['comment'];
-    $count_view_page = $_GET['count_view_page'];
-    $status = $_GET['status'];
-    $code = $_GET['code'];
-    $stmt = $str_exe-update(courses
-    , name = '$name', speaker_name = '$speaker_name', detail = '$detail', img_path = '$img_path', course_outline = '$course_outline', date_open = '$date_open', date_end = '$date_end', place = '$place', seat_num = '$seat_num', cost = '$cost', comment = '$comment', count_view_page = '$count_view_page', status = '$status' 
-    ,where code = .$code);
-    if($stmt)echo json_encode(array('msg'='Update OK'));
-    else echo json_encode(array('msg'='Can not Update'));     
-    break;
-
-
-    case 'delete' 
-    $code = $_GET['code'];
-    $stmt = $str_exe-readOne(DELETE FROM,courses,WHERE code = .$code);
-    if($stmt)echo json_encode(array('msg'='Delete OK'));
-    else echo json_encode(array('msg'='Can not Delete'));
-    break;
-    break;
+    //นับ
+    public function rowCount($tablename){
+        $stmt = $this->conn->prepare("SELECT  COUNT(*) AS total_row FROM ".$tablename);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row['total_row'];  
+     }
+     //ค้นหาตารางมฟิล
+     public function readOne($command,$tablename,$cond){
+        $stmt = $this->conn->prepare(" ".$command."  ".$tablename." ".$cond." ");
+        if($command == "DELETE FROM")
+        {
+            return $this->EXEC($stmt);
+        }
+        if($command == "SELECT * FROM")
+        {
+            $stmt->execute();
+            return $stmt; 
+        } 
+     }
+     public function update($tablename,$filde,$cond)
+     {
+        $stmt = $this->conn->prepare(" UPDATE ".$tablename." SET ".$filde." ".$cond."");
+        return $this->EXEC($stmt);
+     }
+     
+     //เพิ่มข้อมูล
+     public function insert($tablename,$Field,$Value)
+     {
+         $stmt = $this->conn->prepare("INSERT INTO ".$tablename." (".$Field.") VALUES (".$Value.") ");
+         //$stmt->execute();
+         return $this->EXEC($stmt);
+         
+     }
+     public function EXEC($stmt)
+     {
+        if($stmt->execute()) return true;
+        else return false;
+     }
+     
 }
 ?>
-
-
